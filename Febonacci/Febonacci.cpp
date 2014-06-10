23a426733d7372b0d85c6e8f78a3e99be47bf36b
Alex Tremaskin <tremaskin.alexiy@gmail.com>
2014-06-10 13:59:57 +0400

    Simple lib Febonacci

---
+++ Febonacci/Febonacci.cpp
@@ -0,0 +1,22 @@
+#include "Febonacci.h"
+
+int get_febonacci (int index)
+{
+	int prev_value = 1;
+	int current_value = 1;
+
+	if (index == 0)
+		return prev_value;
+
+	if (index == 1)
+		return current_value;
+
+	for (int i = 2; i <= index; i++)
+	{
+		int next = current_value + prev_value;
+		prev_value = current_value;
+		current_value = next;
+	}
+
+	return current_value;
+}
---
+++ Febonacci/Febonacci.h
@@ -0,0 +1,6 @@
+#ifndef _FEBONACCI_H_
+#define _FEBONACHI_H_
+
+int get_febonacci (int index);
+
+#endif//_FEBONACCI_H_
