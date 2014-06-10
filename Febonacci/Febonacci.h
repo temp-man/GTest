34ca086cc2508ad9a7dbbafe3ffb41579e2e2619
Alex Tremaskin <tremaskin.alexiy@gmail.com>
2014-06-10 15:31:45 +0400

    Some comments

--- Febonacci/Febonacci.h
+++ Febonacci/Febonacci.h
@@ -1,6 +1,9 @@
 #ifndef _FEBONACCI_H_
 #define _FEBONACHI_H_
 
+/// @brief get number from febonacci sequence by index.
+/// @param index index in sequence.
+/// @return number from febonacci sequence.
 int get_febonacci (int index);
 
 #endif//_FEBONACCI_H_
